1bdfa1bd14c202e1868d7dd8feb0e8da2987ec12
mandrakevr46 <softpaq@gmail.com>
2024-05-03 16:45:51 -0600

    Se integro la carpeta autocontenedora con archivos

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Inicio de Sistema</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1 class="text-center">Hola Bienvenido al mundo de desarrollo web</h1>
+    <script src="js/bootstrap.min.js></script>
+    </body>
+</html>
